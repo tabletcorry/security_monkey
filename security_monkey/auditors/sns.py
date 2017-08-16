66e99a05a652bdd2b1254ec956940c1f1b3f7557
Patrick Kelley <opticrealm+cloud9@gmail.com>
2017-08-16 01:06:51 +0000

    Updating SNS.

--- security_monkey/auditors/sns.py
+++ security_monkey/auditors/sns.py
@@ -20,23 +20,19 @@
 
 """
 
-from security_monkey.auditor import Auditor
 from security_monkey.watchers.sns import SNS
-from security_monkey.exceptions import InvalidARN
-from security_monkey.exceptions import InvalidSourceOwner
-from security_monkey.datastore import Account
-
+from security_monkey.auditors.resource_policy_auditor import ResourcePolicyAuditor
 from policyuniverse.arn import ARN
-import re
 
 
-class SNSAuditor(Auditor):
+class SNSAuditor(ResourcePolicyAuditor):
     index = SNS.index
     i_am_singular = SNS.i_am_singular
     i_am_plural = SNS.i_am_plural
 
     def __init__(self, accounts=None, debug=False):
         super(SNSAuditor, self).__init__(accounts=accounts, debug=debug)
+        self.policy_keys = ['policy']
 
     def check_snstopicpolicy_empty(self, snsitem):
         """
@@ -67,72 +63,3 @@ class SNSAuditor(Auditor):
             )
             owner = subscription.get('Owner', None)
             self._check_cross_account(owner, snsitem, source)
-
-    def _parse_arn(self, arn_input, account_numbers, snsitem):
-        if arn_input == '*':
-            notes = "An SNS policy where { 'Principal': { 'AWS': '*' } } must also have"
-            notes += " a {'Condition': {'StringEquals': { 'AWS:SourceOwner': '<ARN>' } } }"
-            notes += " or it is open to the world."
-            self.add_issue(10, 'SNS Topic open to everyone', snsitem, notes=notes)
-            return
-
-        arn = ARN(arn_input)
-        if arn.error:
-            self.add_issue(3, 'Auditor could not parse ARN', snsitem, notes=arn_input)
-            return
-
-        if arn.tech == 's3':
-            notes = "SNS allows access from S3 bucket [{}]. ".format(arn.name)
-            notes += "Security Monkey does not yet have the capability to determine if this is "
-            notes += "a friendly S3 bucket.  Please verify manually."
-            self.add_issue(3, 'SNS allows access from S3 bucket', snsitem, notes=notes)
-        else:
-            account_numbers.append(arn.account_number)
-
-    def check_snstopicpolicy_crossaccount(self, snsitem):
-        """
-        alert on cross account access
-        """
-        policy = snsitem.config.get('policy', {})
-        for statement in policy.get("Statement", []):
-            account_numbers = []
-            princ = statement.get("Principal", {})
-            if isinstance(princ, dict):
-                princ_val = princ.get("AWS") or princ.get("Service")
-            else:
-                princ_val = princ
-
-            if princ_val == "*":
-                condition = statement.get('Condition', {})
-                arns = ARN.extract_arns_from_statement_condition(condition)
-
-                if not arns:
-                    tag = "SNS Topic open to everyone"
-                    notes = "An SNS policy where { 'Principal': { 'AWS': '*' } } must also have"
-                    notes += " a {'Condition': {'StringEquals': { 'AWS:SourceOwner': '<ARN>' } } }"
-                    notes += " or it is open to the world. In this case, anyone is allowed to perform "
-                    notes += " this action(s): {}".format(statement.get("Action"))
-                    self.add_issue(10, tag, snsitem, notes=notes)
-
-                for arn in arns:
-                    self._parse_arn(arn, account_numbers, snsitem)
-
-            else:
-                if isinstance(princ_val, list):
-                    for entry in princ_val:
-                        arn = ARN(entry)
-                        if arn.error:
-                            self.add_issue(3, 'Auditor could not parse ARN', snsitem, notes=entry)
-                            continue
-
-                        if not arn.service:
-                            account_numbers.append(arn.account_number)
-                else:
-                    arn = ARN(princ_val)
-                    if arn.error:
-                        self.add_issue(3, 'Auditor could not parse ARN', snsitem, notes=princ_val)
-                    elif not arn.service:
-                        account_numbers.append(arn.account_number)
-
-            for account_number in account_numbers:
-                self._check_cross_account(account_number, snsitem, 'policy')
